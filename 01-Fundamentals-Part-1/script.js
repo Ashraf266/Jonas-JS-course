7a56318a0b4b116a4ec381c52792f27491ccbb16
Ashraf266 <mohamedashrafece@gmail.com>
2025-04-14 16:38:30 +0200

    Fundamentals Part 1_2

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -60,7 +60,114 @@ console.log(multiLineString2);
 
 //if else control structure
 if (true) {
-    console.log(true);
+  console.log(true);
 } else {
-    console.log(false);
+  console.log(false);
 }
+
+// -------------------------------
+
+//type conversion
+
+const inputYear = "1991";
+
+console.log(inputYear + 18); // '199118'
+console.log(Number(inputYear) + 18); //2009
+// or
+console.log(+inputYear + 18); //2009
+
+console.log(Number("Ashraf")); //NaN
+console.log(typeof NaN); //number (invalid number but type is number)
+
+console.log(String(32)); //"32"
+
+const num32 = 32;
+console.log(num32.toString()); //"32"
+
+//we can convert only to Numbers Strings Booleans
+
+//type coercion
+
+console.log("I am " + 32 + "years old"); //concatination triggers string conversion
+console.log("I am " + String(32) + "years old");
+console.log("I am " + "32" + "years old");
+
+console.log("23" - "10" - 3); //subtraction triggers numbers conversion
+console.log("23" + "10" - 3);
+console.log("23" + 10 - 3);
+
+console.log("23" * "2"); //multiplication (NOT LIKE PYTHON) and division triggers numbers conversion
+console.log("23" * 2);
+console.log("22" / "2");
+console.log("22" / 2);
+
+console.log(2 + 3 + 4 + "5"); //'95'
+
+// 5 falsy values: 0, '', undefined, null, NaN
+// anything else is a truthy value even [] & {}
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(""));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+
+console.log(Boolean({})); //true !!
+console.log(Boolean([])); //true !!
+
+const money = 0;
+
+if (money) {
+  console.log("you got money");
+} else {
+  console.log("you are broke");
+}
+
+// strict equality === vs (loose) equality ==
+// strict doesn't do type coercion
+
+console.log(18 === "18"); //false
+console.log(18 == "18"); //true
+
+// Number: return true only if both operands have the same value. +0 and -0 are treated as the same value.
+// If either operand is NaN, return false; so, NaN is never equal to NaN.
+
+console.log(NaN === NaN); //false
+console.log(NaN == NaN); //false
+
+// const favouriteValue = prompt("What's your fav number?"); // to get user input returns a string
+// console.log(favouriteValue);
+
+// logical operators && || !
+
+let testDay = "mon";
+
+switch (testDay) {
+  case 1: // strict equality
+  case 2:
+    //code;
+    break;
+
+  case "mon":
+    //code
+    break;
+
+  default:
+  //code
+}
+
+// statements vs expressions
+
+3 + 4; // is an expressions it produces a value
+1991; // is an expression
+true && false && !true; // is an expression
+
+// if else && switch are a statements we can say statements are like actions
+
+// in template literals JS excpect expressions not statements
+
+`${221 - 332}`; // `${if(true) 1 }` error
+`${true ? 4 : 3}`; // ternary operator considered an expression and not a statement
+
+// to resolve forward compatibility we use "Babel" to transpile or polyfill code
+// (converting it back to ES5 to ensure browser compatibility for all users)
